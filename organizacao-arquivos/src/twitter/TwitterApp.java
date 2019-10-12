3846f616e7a0b9a71a50c36eca1f9e39880d5f5d
NicolasZanatto <34211411+NicolasZanatto@users.noreply.github.com>
2019-10-12 18:35:58 -0300

    bug inserção
    Está com o bug na inserção, alguns valores estão duplicados e não estão sequenciais

--- organizacao-arquivos/src/twitter/TwitterApp.java
+++ organizacao-arquivos/src/twitter/TwitterApp.java
@@ -37,56 +37,65 @@ public class TwitterApp {
         BufferedWriter buffWriter_id = new BufferedWriter(writer_id);
         
         int remainingTweets = 1000;
-        String ultimoIDInserido = "1183072758101663744";
-		try {
-			//while(remainingTweets > 0) {
-				Arquivo arquivo = new Arquivo();
+
+        try {
+			while(remainingTweets > 0) {
 				Query query = new Query("nfl");
 	            query.setCount(100);
 	            QueryResult result = twitter.search(query);
 	            tweets = result.getTweets();
 	            Collections.reverse(tweets);
-	            
-	            int indice = ultimo_indice();
-	            System.out.println("Último índice: " + indice);
-	            
+	            long ultimoIdTwitterInserido = ultimo_idTwitter();
+	            int indice = ultimo_indice();	            
 	            for (Status tweet : tweets) {
-	            	if(!tweet.isRetweeted()) {
-	            		HashtagEntity[] hashtag =  tweet.getHashtagEntities();
-	            		if(hashtag.length > 0)
-	            			System.out.println("Hashtag:" + hashtag[0].getText());
-		                arquivo.setIdtwitter(tweet.getId());
-		                arquivo.setMensagem(tweet.getText());
-		                arquivo.setData(tweet.getCreatedAt());
-	            		System.out.println("id: " + arquivo.getIdTwitter());
-	            		System.out.println("mensagem: " + arquivo.getMensagem());
-	            		System.out.println("data: " + arquivo.getDataFormatada());
-	        			
+	            	if(!tweet.isRetweeted() && tweet.getId() > ultimoIdTwitterInserido) {
 	            		
+	            		Arquivo arquivo = testes_print(tweet);
+	            		//Inserção Arquivo de Dados	
+	            		System.out.println("último indice twitter: " + ultimoIdTwitterInserido);
 	            		buffWriter.write(arquivo.getIdTwitter() + arquivo.getMensagem() + arquivo.getDataFormatada() + "\n");
+	            		ultimoIdTwitterInserido = arquivo.getIdTwitter();
 	            		
+	            		//Inserção Arquivo de Índice
 	            		String string_indicie = Integer.toString(indice);
 	            		buffWriter_id.write(String.format("%6.6s", string_indicie) + arquivo.getIdTwitter() + "\n");
 	            		System.out.println("indice: " + indice);
 	            		indice++;
 	            	}
+	            	System.out.println("");
 	            }
-	            writer.flush();
-	            buffWriter.close();
-	            writer.close();
-	            
-	            writer_id.flush();
-	            buffWriter_id.close();
-	            writer_id.close();
-	            
-	            remainingTweets--;
-			//}
+	            remainingTweets = remainingTweets - tweets.size();
+			}
+			
+			writer.flush();
+            buffWriter.close();
+            writer.close();
+            
+            writer_id.flush();
+            buffWriter_id.close();
+            writer_id.close();
             
         } catch (TwitterException te) {
             te.printStackTrace();
             System.out.println("Failed to search tweets: " + te.getMessage());
             System.exit(-1);
         }
+	}
+
+	private static Arquivo testes_print(Status tweet) {
+		HashtagEntity[] hashtag =  tweet.getHashtagEntities();
+		if(hashtag.length > 0)
+			System.out.println("Hashtag:" + hashtag[0].getText());
+		Arquivo arquivo = new Arquivo();
+		arquivo.setIdtwitter(tweet.getId());
+        arquivo.setMensagem(tweet.getText());
+        arquivo.setData(tweet.getCreatedAt());
+		System.out.println("id: " + arquivo.getIdTwitter());
+		System.out.println("mensagem: " + arquivo.getMensagem());
+		System.out.println("data: " + arquivo.getDataFormatada());
+		
+		return arquivo;
+	
 	}
 
 	public static int ultimo_indice()
@@ -124,5 +133,44 @@ public class TwitterApp {
 		ultimo = ultimo.trim();
 	
 		return Integer.parseInt(ultimo);
+	}
+	
+	public static long ultimo_idTwitter()
+	{	
+	
+		String ultimo = "0";
+		try {
+			InputStream file = new FileInputStream("teste.txt");
+			InputStreamReader file_reader = new InputStreamReader(file);
+			BufferedReader buffer = new BufferedReader(file_reader);
+	
+			String line = "";
+			while (line != null) {
+				line = buffer.readLine();
+				if (line != null) {
+					ultimo = line;
+				}
+			}
+			
+			if (ultimo.equals(String.valueOf('0')))
+			{
+				
+			}
+			else
+			{
+				ultimo = ultimo.substring(0,19);
+			}
+	
+			 	
+			buffer.close();
+			file_reader.close();
+			file.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		ultimo = ultimo.trim();
+	
+		return Long.parseLong(ultimo);
 	}
 }
